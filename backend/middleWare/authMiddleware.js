7b0495d5427a44cdf00cb6bb9d90d97cb9c4ee61
Kavindi Arangala <95092298+Achinika@users.noreply.github.com>
2023-04-30 22:41:30 +0530

    Create authMiddleware.js

---
+++ backend/middleWare/authMiddleware.js
@@ -0,0 +1,15 @@
+const asyncHandler = require("express-async-handler");
+const Inventory_Item = require("../models/inventoryItemModel");
+const jwt = require("jsonwebtoken");
+
+
+
+const protect = asyncHandler(async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+module.exports = protect
